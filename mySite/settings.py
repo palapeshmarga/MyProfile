928b02ca15c98aa2c1b24468e93fdf074dcdaec4
palapeshmarga <palapeshmarga4@gmail.com>
2026-07-22 22:09:00 +0300

    Added media routes and fixed profile image positioning

--- mySite/settings.py
+++ mySite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(o5smi*q4wy-pgh*7wp%@+(g19ctdy2qoniab5+w-a_g6poje='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['palapeshmarga.pythonanywhere.com','127.0.0.1', 'localhost']
 
