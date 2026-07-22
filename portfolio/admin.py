32db273ce9aa9b1e7bbc572bc5e6647b555b3753
palapeshmarga <palapeshmarga4@gmail.com>
2026-07-22 19:18:28 +0300

    Ensure portfolio app is registered in settings and admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,11 +3,9 @@ from .models import Project, ProjectMedia
 
 class ProjectMediaInline(admin.TabularInline):
     model = ProjectMedia
-    extra = 1  # Provides an extra empty row to upload files right away
+    extra = 2
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'technology', 'created_at')
     inlines = [ProjectMediaInline]
-
-admin.site.register(ProjectMedia)
