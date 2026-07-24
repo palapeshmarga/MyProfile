9395e0aaefc51eac264ae8446412b3a81f341b03
palapeshmarga <palapeshmarga4@gmail.com>
2026-07-24 04:24:22 +0300

    Matched template variable names to models

--- mySite/urls.py
+++ mySite/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('portfolio.urls')),
 ]
 
-# Serves media files (uploaded project photos/GIFs)
+# Serves uploaded media during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
